4a69e42aca1d780ca17f7c8e5bc68869797d2204
Akech-ann <akechannatong@gmail.com>
2023-02-15 22:54:57 +0300

    assignment on strings

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val school = "akirachix"
+    println(school[0])
+    println(school[2])
+    println(school[3])
+    printname("Ann",20)
+    getlength("My name is Ann")
+    call("bro")
+}
+fun printname(name:String,age:Int):String{
+    val sentence = "Hi,my name is $name and I am $age years old"
+    return sentence
+
+}
+
+fun getlength(length1:String):Int{
+    val length = length1.length
+    return length
+}
+fun call(text:String){
+    val b = "bro"
+    if (text.equals(b))
+    {
+        println("That's me")
+}
+    else{
+    println("I don't know who that is")
+    }
+}
+
+
+
+
